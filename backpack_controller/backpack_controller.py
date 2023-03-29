0acad9e2d81e53ed319c749283bd5248985979b6
Matěj Boxan <matej.boxan@gmail.cz>
2023-03-29 10:15:49 -0400

    Update main script

--- backpack_controller/backpack_controller.py
+++ backpack_controller/backpack_controller.py
@@ -11,17 +11,24 @@ from tf2_ros.transform_listener import TransformListener
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import os
+import time
+from threading import Lock
+
 class BackpackController(Node):
 
+    frequency = 1500
+    base_length = 10.0
+    base_duration = 0.1
     dist_rate_dict = {5.0: 1.0,
-                      4.0: 0.7,
-                      0.5: 0.5,
-                      0.1: 0.3,
-                      0.05: 0.1,
-                      0.0: 0.0}
+                      4.0: 0.8,
+                      3.0: 0.6,
+                      2.0: 0.5,
+                      1.0: 0.4,
+                      0.5: 0.2,
+                      0.0: 0.1}
 
     def __init__(self):
-        super().__init__('minimal_publisher')
+        super().__init__('backpack_controller')
         self.publisher_ = self.create_publisher(String, 'distance_to_path', 10)
         self.path_subscriber_ = self.create_subscription(Path, 'planned_trajectory_test',
                                                          self.path_callback, 10)
@@ -33,41 +40,49 @@ class BackpackController(Node):
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
-        beeper_period = 0.1
-        timer_period = 1.0  # seconds
+        beeper_period = self.base_length
+        distance_update_period = 1.0  # seconds
         self.beeper_timer = self.create_timer(beeper_period, self.beeper_callback)
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.distance_to_traj = 0
-        self.path = None
+        self.distance_update_timer = self.create_timer(distance_update_period, self.timer_callback)
+        self.distance_to_traj = None
         self.last_pos = None
         self.knn = NearestNeighbors(n_neighbors=1)
+        self.ratio = None
+        self.path = None
+        self.ratio_mutex = Lock()
         self.get_logger().info("Waiting for path message")
-        self.sound_ratio = 0.0
-        self.new_sound_ratio = 0.0
-        self.new_ratio_ready = False
-        self.sound_ctr = 0.0
-        self.sound = None
 
-    def play_sound(self):
-        duration = 100
-        frequency = 1500
-        os.system('play -n synth %s sin %s' % (duration/1000, frequency))
+    def play_sound(self, sound_dur, pause_dur, count):
+        if sound_dur * count == self.base_length:
+            os.system(f'play -n synth {self.base_length} sin {self.frequency} >/dev/null 2>&1')
+        elif pause_dur * count == self.base_length:
+            time.sleep(self.base_length)
+        else:
+            for i in range(count):
+                os.system(f'play -n synth {sound_dur} sin {self.frequency} >/dev/null 2>&1')
+                time.sleep(pause_dur)
 
     def beeper_callback(self):
-        if self.path is None:
+        if self.path is None or self.ratio is None:
             return
-        if self.new_ratio_ready and self.sound_ctr == 0.0:
-            print(f"new sound ratio ready: {self.sound_ratio}, new: {self.new_sound_ratio}")
-            self.sound_ratio = self.new_sound_ratio
 
-        if self.sound_ctr < self.sound_ratio:
-            self.play_sound()
-            print(f"playing sound {self.sound_ctr}")
+        self.ratio_mutex.acquire()
+        sound_dur_total = self.ratio * self.base_length
+        sound_count = sound_dur_total / self.base_duration
+        sound_dur_indiv = self.base_duration
+
+        pause_dur_total = self.base_length - sound_dur_total
+        if sound_count == 0.0:
+            pause_count = self.base_length
         else:
-            print(f'not playing sound {self.sound_ctr}')
-        self.sound_ctr += 0.1
-        if self.sound_ctr >= 1.0:
-            self.sound_ctr = 0.0
+            pause_count = sound_count
+        pause_dur_indiv = pause_dur_total / pause_count
+
+        print(f"Playing sound for distance {self.distance_to_traj} with ratio {self.ratio}: sound_dur_ind {sound_dur_indiv}, pause_dur_ind {pause_dur_indiv}")
+        print(f"Total sound dur {sound_dur_total} with count {sound_count} total pause dur {pause_dur_total} with count {pause_count}")
+
+        self.ratio_mutex.release()
+        self.play_sound(sound_dur_indiv, pause_dur_indiv, int(sound_count))
 
     def timer_callback(self):
         # get latest TF
@@ -80,25 +95,30 @@ class BackpackController(Node):
                 rclpy.time.Time())
         except TransformException as ex:
             self.get_logger().warn(f'Could not transform {self.target_frame} to map: {ex}')
+            self.ratio_mutex.acquire()
+            self.ratio = None
+            self.ratio_mutex.release()
             return
 
         self.last_pos = tf.transform.translation
         self.distance_to_traj = self.compute_dist_to_traj(
             np.array([self.last_pos.x, self.last_pos.y]))
+        self.ratio_mutex.acquire()
+        self.ratio = 0.1
         for i in range(len(self.dist_rate_dict) - 1):
-            if list(self.dist_rate_dict.keys())[i] > self.distance_to_traj >= \
-                    list(self.dist_rate_dict.keys())[i + 1]:
-                self.new_sound_ratio = list(self.dist_rate_dict.values())[i + 1]
-        if self.new_sound_ratio != self.sound_ratio:
-            self.new_ratio_ready = True
-        else:
-            self.new_ratio_ready = False
+            if self.distance_to_traj >= list(self.dist_rate_dict.keys())[i]:
+                self.ratio = list(self.dist_rate_dict.values())[i]
+                break
+        self.ratio_mutex.release()
         msg = String()
         msg.data = f'Distance to target: {self.distance_to_traj}'
         self.publisher_.publish(msg)
         self.get_logger().info(msg.data)
 
     def path_callback(self, path):
+        if not path.poses:
+            self.get_logger().warn("Received empty path, skipping")
+            return
         self.path = path
 
         poses = self.path.poses
@@ -128,3 +148,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
